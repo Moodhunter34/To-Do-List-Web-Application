7c75e3779f943c253cffe2e7526a80944b573abb
Nik-Pap <nikospap8@hotmail.com>
2021-03-11 12:18:54 +0000

    first commit

---
+++ src/main/java/com/example/duckdemo/ToDoListWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.duckdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoListWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoListWebApplication.class, args);
+	}
+
+}
